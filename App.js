b5d265c9138956776cff58a5f03c24687d4a9a9c
Märta Ballardini <marta.ballardini@isotop.se>
2023-01-20 16:38:19 +0100

    initial

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { WebView } from "react-native-webview";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeAreaView style={{ flex: 0, backgroundColor: "#b0a0ed" }} />
+      <SafeAreaView style={{ flex: 1, backgroundColor: "#7996e9" }}>
+        <WebView
+          style={{ flex: 1 }}
+          useWebKit={true}
+          source={{ url: "https://karatefylla.vercel.app" }}
+        />
+        <Text style={{ textAlign: "center", fontSize: "12px", padding: "5%" }}>
+          Produkt av Märta Ballardini, Wilma Stenström och Linnea Gustafsson
+        </Text>
+      </SafeAreaView>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
