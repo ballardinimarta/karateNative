5e4fa3d0e2749fe2f3dbc99a8c9cb02d7a943826
ballardinimarta <marta.ballardini@gmail.com>
2023-01-23 20:43:59 +0100

    conf

--- App.js
+++ App.js
@@ -1,9 +1,18 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import React, { useState } from "react";
+import { Text, View, SafeAreaView, Image } from "react-native";
 import { WebView } from "react-native-webview";
-import { LinearGradient } from "expo-linear-gradient";
+
+const ActivityIndicatorElement = () => {
+  return (
+    <View style={{ flex: 0, backgroundColor: "#b0a0ed" }}>
+      <Image source={require("./assets/splash.png")} />
+    </View>
+  );
+};
 
 export default function App() {
+  const [visible, setVisible] = useState(false);
+
   return (
     <>
       <SafeAreaView style={{ flex: 0, backgroundColor: "#b0a0ed" }} />
@@ -12,8 +21,20 @@ export default function App() {
           style={{ flex: 1 }}
           useWebKit={true}
           source={{ url: "https://karatefylla.vercel.app" }}
+          javaScriptEnabled={true}
+          domStorageEnabled={true}
+          onLoadStart={() => setVisible(true)}
+          onLoad={() => setVisible(false)}
         />
-        <Text style={{ textAlign: "center", fontSize: "12px", padding: "5%" }}>
+        {visible ? <ActivityIndicatorElement /> : null}
+
+        <Text
+          style={{
+            textAlign: "center",
+            fontSize: "12px",
+            padding: "5%",
+          }}
+        >
           Produkt av Märta Ballardini, Wilma Stenström och Linnea Gustafsson
         </Text>
       </SafeAreaView>
